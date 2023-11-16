760c1ea0630810e3a6deeb1413c696a7f8eb8882
benisprlh <benisaprulah5@gmail.com>
2023-11-17 05:16:23 +0700

    feat: add midtrans

--- Server/auth/authorization.js
+++ Server/auth/authorization.js
@@ -1,6 +1,6 @@
 async function authorization(req, res, next) {
   try {
-    if (req.user.role === 'Admin') {
+    if (req.user.role === 'Admin' || req.user.role === 'Premium') {
       return next();
     } else {
       throw { name: 'forbidden' };
